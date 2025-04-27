b8ded624e4dabb6299aa1c363dcab93a03732747
Tengz <redcraftrus@gmail.com>
2025-04-27 18:08:37 +0300

    feat: improved import validations

--- internal/getters.go
+++ internal/getters.go
@@ -6,13 +6,21 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func getApiVerLink(version uint8) string {
+	if version < 1 || version > 2 {
+		panic("Invalid version number " + strconv.Itoa(int(version)))
+	}
 	return fmt.Sprintf("/tx_api/v%d", version)
 }
 
 func getApiEnvHost(apiEnv string) string {
+	if apiEnv == "" {
+		panic("No API environment specified")
+	}
+
 	domainPart := "threatx.io"
 	oldHostParts := map[string]string{
 		"prod":    "",
@@ -22,7 +30,12 @@ func getApiEnvHost(apiEnv string) string {
 		"staging": "staging0",
 	}
 	if _, ok := oldHostParts[apiEnv]; ok {
-		return fmt.Sprintf("https://api%s.%s", oldHostParts[apiEnv], domainPart)
+		delimiter := ""
+		if apiEnv != "prod" {
+			delimiter = "-"
+		}
+
+		return fmt.Sprintf("https://api%s%s.%s", delimiter, oldHostParts[apiEnv], domainPart)
 	}
 
 	return fmt.Sprintf("https://%s.%s", apiEnv, domainPart)
