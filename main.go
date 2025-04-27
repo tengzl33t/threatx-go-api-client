0fea361f423d335072a4766ee6cab5a1c1c31572
Tengz <redcraftrus@gmail.com>
2025-04-27 18:08:50 +0300

    chore: cleanup

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import "C"
 import (
-	"log"
 	"threatx-go-api-client/pkg"
-	"time"
 )
 
-func timeTrack(start time.Time) {
-	elapsed := time.Since(start)
-	log.Printf("took %s", elapsed)
-}
-
 func main() {
-	defer timeTrack(time.Now())
 	pkg.RunClient(
 		"sites",
 		"prod",
